e9d111dcea3dd3b9a18ea080dbc5e529a003985f
Dillon Scott <dillonscott.2024@gmail.com>
2021-11-28 13:10:55 -0500

    generalized shell type header

---
+++ src/components/ShellType.tsx
@@ -0,0 +1,60 @@
+import React, { ReactElement } from "react";
+import styled, { Keyframes, keyframes } from "styled-components";
+
+
+const StyledH2 = styled.h2`
+  ::after {
+    animation: ${props => props.color ? (
+    animation(props.color)
+  ) : (
+    'none'
+  )} 3s ease-in-out 0.75s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+    content: ${props => props.color ? (
+    "'" + ' '.repeat(props.color.length + 1) + "'"
+  ) : (
+    "'error'"
+  )};
+    white-space: pre;
+  }
+`;
+
+const animation = (text: string): Keyframes => {
+  var animationStr = "";
+  const frameRate = (100 / (text.length + (text.length % 2 === 0 ? 1 : 0.5)));
+  for (let i = 0; i <= text.length; i++) {
+    if (i % 2 === 0) {
+      animationStr += `
+      ${(frameRate * i).toFixed(2)}% {content: '${text.substring(0, i) + ' '.repeat(text.length - i + 1)}'}`;
+      animationStr += `
+      ${(frameRate * i + frameRate / 2).toFixed(2)}% {content: '${text.substring(0, i)}_${i < text.length ? ' '.repeat(text.length - i) : ''}'}`;
+    } else {
+      animationStr += `
+      ${(frameRate * i).toFixed(2)}% {content: '${text.substring(0, i)}_${i < text.length ? ' '.repeat(text.length - i) : ''}'}`;
+      animationStr += `
+      ${(frameRate * i + frameRate / 2).toFixed(2)}% {content: '${text.substring(0, i) + ' '.repeat(text.length - i + 1)}'}`;
+    }
+  }
+  if (text.length % 2 === 0) animationStr += `
+  100% {content: '${text + ' '}'}`;
+  console.log(text);
+  console.log(animationStr);
+  return keyframes`${animationStr}`;
+}
+
+const ShellType = ({
+  text,
+  className = "",
+}: {
+  text: string,
+  className?: string,
+}): ReactElement => {
+  return (
+    <StyledH2 className={className} color={text}>
+      &gt;&nbsp;
+    </StyledH2>
+  );
+}
+
+export default ShellType;
