de997ae07ea77c0ff02454b2eb45286f7b27da9a
Dillon Scott <dillonscott.2024@gmail.com>
2021-11-28 14:22:18 -0500

    add image

--- src/tabs/AboutMe.tsx
+++ src/tabs/AboutMe.tsx
@@ -1,42 +1,11 @@
-import React, { ReactElement } from 'react';
-import styled, { keyframes } from 'styled-components';
+import React, { ReactElement, useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { ReactComponent as MailIcon } from '../assets/mail.svg';
 import { ReactComponent as LinkedinIcon } from '../assets/linkedin.svg';
 import { ReactComponent as GithubIcon } from '../assets/github.svg';
-
-const contactAnimation = keyframes`
-  0% { content: '_'}
-  6.25% {content: 'C_' }
-  12.5% { content: 'Co' }
-  18.75% { content: 'Con' }
-  31.25% { content: 'Con_' }
-  37.5% { content: 'Cont_' }
-  43.75% { content: 'Conta' }
-  50% { content: 'Contac' }
-  56.25% { content: 'Contac_' }
-  62.5% { content: 'Contact_' }
-  68.75% { content: 'Contact' }
-  75% { content: 'Contact' }
-  81.25% { content: 'Contact_' }
-  87.5% { content: 'Contact_' }
-  93.75% { content: 'Contact' }
-  100% { content: 'Contact' }
-`
-
-const Contact = styled.h2`
-  align-self: center;
-  font-family: Roboto Mono;
-  justify-self: self-start;
-  margin-top: 0;
-  width: 8rem;
-
-  ::after {
-    animation: ${contactAnimation} 6s ease-in-out 0.75s;
-    animation-iteration-count: 1;
-    animation-fill-mode: forwards;
-    content: '';
-  }
-`;
+import ScottJpgHiRes from '../assets/scottHiRes.jpg';
+import ScottJpgLowRes from '../assets/scottLowRes.jpg';
+import ShellType from '../components/ShellType';
 
 const Link = styled.a`
   align-items: center;
@@ -46,20 +15,25 @@ const Link = styled.a`
   display: flex;
   flex-direction: row;
   justify-self: center;
-  margin: 0.5rem 0; 
+  margin: 0.5rem 0;
+  text-decoration: underline;
+  text-decoration-color: transparent;
+  transition-duration: 0.5s;
   width: 100%;
 
-  :link, :visited {
-    text-decoration: none;
-  }
-
   :hover {
-    text-decoration: underline;
+    text-decoration-color: black;
+
+    svg {
+      opacity: 100%;
+    }
   }
 
   svg {
     align-self: flex-start;
-    margin-right: 0.5rem;
+    margin-right: 0.75rem;
+    opacity: 50%;
+    transition-duration: 0.5s;
   }
 `;
 
@@ -71,8 +45,9 @@ const LinkColumns = styled.div`
 
 const LinkText = styled.p`
   align-self: center;
-  justify-self: center;
-  margin: auto;
+  font-family: Montserrat;
+  justify-self: flex-start;
+  margin: 0;
 `;
 
 const NameHeader = styled.h1`
@@ -82,13 +57,63 @@ const NameHeader = styled.h1`
   margin-top: 2rem;
 `;
 
+const SelfImage = styled.img`
+  border-radius: 50%;
+  box-shadow: 0.2rem 0.2rem 1rem gray;
+  height: auto;
+  max-width: 15%;
+`;
+
+const StyledShellType = styled(ShellType)`
+  align-self: center;
+  font-family: Roboto Mono;
+  margin: 1rem;
+`;
+
+const WhoIAm = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+const WhoIAmText = styled.p`
+  align-self: center;
+  font-family: Montserrat;
+  max-width: 50%;
+`;
+
+// https://benhoneywill.com/progressive-image-loading-with-react-hooks/
+const useProgressiveImg = (lowQualitySrc: string, highQualitySrc: string): [string, { blur: boolean }] => {
+  const [src, setSrc] = useState(lowQualitySrc);
+  useEffect(() => {
+    setSrc(lowQualitySrc);
+    const img = new Image();
+    img.src = highQualitySrc;
+    img.onload = () => {
+      setSrc(highQualitySrc);
+    };
+  }, [lowQualitySrc, highQualitySrc]);
+  return [src, { blur: src === lowQualitySrc }];
+};
+
 const AboutMe = (): ReactElement => {
+  const [src, { blur }] = useProgressiveImg(ScottJpgLowRes, ScottJpgHiRes);
   return (
     <>
       <NameHeader>Dillon Scott</NameHeader>
-      <Contact>
-        &gt;&nbsp;
-      </Contact>
+      <StyledShellType text="Who I Am" delay="1s" />
+      <WhoIAm>
+        <WhoIAmText>
+          // TODO: Fill in this section
+        </WhoIAmText>
+        <SelfImage src={src} alt="A picture of me!"
+          style={{
+            filter: blur ? "blur(20px)" : "none",
+            transition: blur ? "none" : "filter 1s ease-out"
+          }} />
+      </WhoIAm>
+      <StyledShellType text="Contact" delay="4s" />
       <LinkColumns>
         <Link href="mailto:scott.di@northeastern.edu" target="_blank">
           <MailIcon />
@@ -103,6 +128,8 @@ const AboutMe = (): ReactElement => {
           <LinkText>github.com/dillydally414</LinkText>
         </Link>
       </LinkColumns>
+      <StyledShellType text="Resume" delay="7s" />
+      // TODO: Add resume + make it downloadable on click
     </>
   );
 };
